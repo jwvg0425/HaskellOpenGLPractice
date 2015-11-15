2f78208f0182c7549cb23f1b0a2b1bfe2b47943e
namhyeonuk <jwvg0425@naver.com>
2015-11-15 19:56:54 +0900

    002

--- Practice/src/Main.hs
+++ Practice/src/Main.hs
@@ -26,6 +26,26 @@ module Main (
 
 import Graphics.Rendering.OpenGL as GL
 import Graphics.UI.GLFW as GLFW
+import Control.Monad
+import System.Exit ( exitSuccess )
+
+resizeWindow :: GLFW.WindowSizeCallback
+resizeWindow win w h = do
+    GL.viewport $= (GL.Position 0 0, GL.Size (fromIntegral w) (fromIntegral h))
+    GL.matrixMode $= GL.Projection
+    GL.loadIdentity
+    GL.ortho2D 0 (realToFrac w) (realToFrac h) 0
+
+keyPressed :: GLFW.KeyCallback
+keyPressed win GLFW.Key'Escape _ GLFW.KeyState'Pressed _ = shutdown win
+keyPressed _   _               _ _                     _ = return ()
+
+shutdown :: GLFW.WindowCloseCallback
+shutdown win = do
+    GLFW.destroyWindow win
+    GLFW.terminate
+    exitSuccess
+    return ()
 
 main :: IO ()
 main = do
@@ -33,6 +53,9 @@ main = do
     GLFW.defaultWindowHints
     Just win <- GLFW.createWindow 640 480 "GLFW Demo" Nothing Nothing
     GLFW.makeContextCurrent (Just win)
+    GLFW.setWindowSizeCallback win (Just resizeWindow)
+    GLFW.setKeyCallback win (Just keyPressed)
+    GLFW.setWindowCloseCallback win (Just shutdown)
     onDisplay win
     GLFW.destroyWindow win
     GLFW.terminate
@@ -42,4 +65,7 @@ onDisplay win = do
     GL.clearColor $= Color4 1 0 0 1
     GL.clear [ColorBuffer]
     GLFW.swapBuffers win
-    onDisplay win
+
+    forever $ do
+        GLFW.pollEvents
+        onDisplay win
